e13715d4fc26885637f98a9b5c2c8c2d20586235
fadlirizaldy <fadli.rizaldi12@gmail.com>
2023-03-25 19:02:15 +0700

    fix react form

--- 19_React Form/praktikum/LandingPage/src/components/Login/Login.jsx
+++ 19_React Form/praktikum/LandingPage/src/components/Login/Login.jsx
@@ -2,16 +2,12 @@ import { useLogin } from "../../contexts/LoginContext";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  //   const state = useLocation();
-
-  //   console.log(state);
-
   const { dispatch } = useLogin();
   const navigate = useNavigate();
 
   const handleLogin = (e) => {
     dispatch({ type: "setIsLogin", payload: true });
-    localStorage.setItem("isSignup", true);
+    // localStorage.setItem("isSignup", true);
     navigate(`/product`);
   };
 
@@ -19,14 +15,14 @@ const Login = () => {
     <div className="container my-4 vh-100 d-flex justify-content-center">
       <form className="w-50 p-4 border h-75" onSubmit={handleLogin}>
         <div className="form-outline mb-4 ">
-          <input type="email" id="form2Example1" className="form-control" />
+          <input type="email" id="form2Example1" className="form-control" placeholder="Still optional" />
           <label className="form-label" htmlFor="form2Example1">
             Email address
           </label>
         </div>
 
         <div className="form-outline mb-4">
-          <input type="password" id="form2Example2" className="form-control" />
+          <input type="password" id="form2Example2" className="form-control" placeholder="Still optional" />
           <label className="form-label" htmlFor="form2Example2">
             Password
           </label>
